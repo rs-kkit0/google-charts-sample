265b416b852b64d9006fd7a0f1c1d5ca7ec688dd
rs-kkit0 <38030070+rs-kkit0@users.noreply.github.com>
2020-06-28 23:07:29 +0900

    修正

--- src/app/component/comfirmation/comfirmation.component.ts
+++ src/app/component/comfirmation/comfirmation.component.ts
@@ -9,26 +9,53 @@ export class ComfirmationComponent implements OnInit {
 
   incomeTotal = 0;
 
-  incomeList: { caption: string, amount: number }[] = [
-    { caption: '給与（夫・手取り）', amount: 35 },
-    { caption: '給与（妻・手取り）', amount: 8.6 },
-    { caption: 'ボーナス（夫・手取り）', amount: 100 },
+  incomeList = [
+    { caption: '給与<br>（夫・手取り)', amount: 35, calcTarget: true },
+    { caption: '給与<br>（妻・手取り）', amount: 8.6, calcTarget: true },
+    { caption: 'ボーナス<br>（夫・手取り）', amount: 100, calcTarget: false },
   ];
 
   expenseTotal = 0;
 
   expenseList = [
-    { caption: '食費', amount: 7.3 },
-    { caption: '電気・ガス・水道', amount: 3.38 },
-    { caption: '交際費', amount: 0.4 }
+    { caption: '食費', amount: 7.3, calcTarget: true },
+    { caption: '電気・ガス・水道', amount: 3.38, calcTarget: true },
+    { caption: '交際費', amount: 0.4, calcTarget: true }
   ]
+
+  savings = 0;
+
+  savingsList = [
+    { caption: '普通預金', amount: 163, calcTarget: true },
+    { caption: '定期預金', amount: 161, calcTarget: true }
+  ]
+
+  investment = 0;
+
+  investmentList = [];
 
   constructor() { }
 
   ngOnInit() {
-    this.incomeTotal = 0;
     this.incomeList.forEach(e => {
-      this.incomeTotal += e.amount;
+      if (e.calcTarget) {
+        this.incomeTotal += e.amount;
+      }
+    });
+    this.expenseList.forEach(e => {
+      if (e.calcTarget) {
+        this.expenseTotal += e.amount;
+      }
+    });
+    this.savingsList.forEach(e => {
+      if (e.calcTarget) {
+        this.savings += e.amount;
+      }
+    });
+    this.investmentList.forEach(e => {
+      if (e.calcTarget) {
+        this.investment += e.amount;
+      }
     });
   }
 
